a7e8093474d687e3a2e8ec1d803b5b6201ebad37
sok205 <Filip007lol@gmail.com>
2025-05-16 15:55:03 +0200

    login register works

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    """
+    Renders the home page.
+    :param request:
+    :return:
+    """
+    return render(request, 'stock/home.html')
