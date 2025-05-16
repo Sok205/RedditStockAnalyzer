f368aaabf1da6ead6dc3130a69fde5adfa21b064
sok205 <Filip007lol@gmail.com>
2025-05-16 15:44:16 +0200

    App inicialization

---
+++ user/views.py
@@ -0,0 +1,41 @@
+"""
+user/views.py
+"""
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+def register(request):
+    """
+    Handles user registration.
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    """
+    Handles user login.
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
